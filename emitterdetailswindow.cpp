d05cf93ee9eed055db26cf5a77d60ac114a02332
Antonio Alarcon <antonio.alarcon@estudiantat.upc.edu>
2021-12-20 02:16:37 +0100

    fix bug detalle emisor

--- emitterdetailswindow.cpp
+++ emitterdetailswindow.cpp
@@ -10,14 +10,18 @@ EmitterDetailsWindow::EmitterDetailsWindow(QWidget *parent, Emitter *emitter) :
     ui->setupUi(this);
 
     int i = 0;
+    WGS84Coordinates middleCoord = WGS84Coordinates(41.29760688636795, 2.0837311365820046);
 
-    for (WGS84Coordinates coord : emitter_->pointsCat21) {
+    if (emitter_->pointsCat21.length() != 0) {
+        for (WGS84Coordinates coord : emitter_->pointsCat21) {
 
-        this->ui->detailsMap->AddCircleMarker(coord,10,"red",QString::number(i));
-        i++;
+            this->ui->detailsMap->AddCircleMarker(coord,10,"red",QString::number(i));
+            i++;
+        }
+        middleCoord = emitter_->pointsCat21.at(emitter->pointsCat21.length()/2);
     }
 
-    WGS84Coordinates middleCoord = emitter_->pointsCat21.at(emitter->pointsCat21.length()/2);
+
     this->ui->detailsMap->SetPosition(middleCoord);
     this->ui->detailsMap->SetZoom(7);
 
@@ -28,6 +32,9 @@ EmitterDetailsWindow::EmitterDetailsWindow(QWidget *parent, Emitter *emitter) :
 
     this->ui->detailsList->addItem("First Packet: " + emitter_->GetFirstReport().toString("hh:mm:ss:zzz"));
     this->ui->detailsList->addItem("Last Packet: " + emitter_->GetLastReport().toString("hh:mm:ss:zzz"));
+    this->ui->detailsList->addItem("Number of Packets of CAT10 SMR: " + QString::number(emitter_->pointsCat10Smr.length()));
+    this->ui->detailsList->addItem("Number of Packets of CAT10 MLAT: " + QString::number(emitter_->pointsCat10Mlat.length()));
+    this->ui->detailsList->addItem("Number of Packets of CAT20: " + QString::number(emitter_->pointsCat20.length()));
     this->ui->detailsList->addItem("Number of Packets of CAT21: " + QString::number(emitter_->pointsCat21.length()));
 
 }
